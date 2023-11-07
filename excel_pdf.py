83f4b4b70cf118edd46dfab52f01a4f26569b949
ceewa30 <ceewa30.gmail.com>
2023-11-07 11:54:31 -0500

    Initial commit

---
+++ excel_pdf.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob('invoice/*.xlsx')
+
+for filepath in filepaths:
+    
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split("-")
+
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w = 80, h = 10, txt=f"Invoice nr. {invoice_nr}", border = 0, ln = 1, align = 'L', fill = False)
+
+    # pdf.set_font(family='Times', style='B', size=16)
+    # pdf.cell(w = 80, h = 10, txt=f"Date: {date}", border = 0, ln = 1, align = 'L', fill = False)
+
+    # df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    # columns = list(df.columns)
+    # columns = [item.replace("_", " ").title() for item in columns]
+
+    # pdf.set_font(family='Times', style='B', size=12)
+    # pdf.set_text_color(80, 80, 80)
+    # pdf.cell(w = 25, h = 8, txt=str(columns[0]), border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 70, h = 8, txt=str(columns[1]), border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 40, h = 8, txt=str(columns[2]), border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 30, h = 8, txt=str(columns[3]), border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 25, h = 8, txt=str(columns[4]), border = 1, ln = 1, align = 'L', fill = False)
+
+    # for index, row in df.iterrows():
+        
+    #     pdf.set_font(family='Times', size=12)
+    #     pdf.set_text_color(80, 80, 80)
+    #     pdf.cell(w = 25, h = 8, txt=str(row['product_id']), border = 1, align = 'L', fill = False)
+    #     pdf.cell(w = 70, h = 8, txt=str(row['product_name']), border = 1, align = 'L', fill = False)
+    #     pdf.cell(w = 40, h = 8, txt=str(row['amount_purchased']), border = 1, align = 'L', fill = False)
+    #     pdf.cell(w = 30, h = 8, txt=str(row['price_per_unit']), border = 1, align = 'L', fill = False)
+    #     pdf.cell(w = 25, h = 8, txt=str(row['total_price']), border = 1, ln = 1, align = 'L', fill = False)
+
+    # total_sum = df.total_price.sum()
+    # pdf.set_font(family='Times', size=12)
+    # pdf.set_text_color(80, 80, 80)
+    # pdf.cell(w = 25, h = 8, txt="", border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 70, h = 8, txt="", border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 40, h = 8, txt="", border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 30, h = 8, txt="", border = 1, align = 'L', fill = False)
+    # pdf.cell(w = 25, h = 8, txt=str(total_sum), border = 1, ln = 1, align = 'L', fill = False)
+
+    # # Add total sum sentence
+    # pdf.set_font(family='Times', style='B', size=12)
+    # pdf.cell(w = 25, h = 8, txt=f"The total price is {str(total_sum)}", ln = 1, align = 'L', fill = False)
+
+    # # Add company name and logo
+    # pdf.set_font(family='Times', style='B', size=12)
+    # pdf.cell(w = 25, h = 8, txt=f"PythonHow", ln = 1, align = 'L', fill = False)
+    # pdf.image("pythonhow.png", w=10)
+
+    pdf.output(f'pdfs/{filename}.pdf')
