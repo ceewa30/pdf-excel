e772a9b1719c4a318ad9c08d364b6f7a4fce89b3
ceewa30 <ceewa30.gmail.com>
2023-11-07 17:34:43 -0500

    Adding table, total price, company logo

--- excel_pdf.py
+++ excel_pdf.py
@@ -14,7 +14,7 @@ for filepath in filepaths:
     invoice_nr, date = filename.split("-")
 
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w = 80, h = 10, txt=f"Invoice nr. {invoice_nr}", border = 0, ln = 1, align = 'L', fill = False)
+    pdf.cell(w = 80, h = 10, txt=f"Invoice no. {invoice_nr}", border = 0, ln = 1, align = 'L', fill = False)
 
     pdf.set_font(family='Times', style='B', size=16)
     pdf.cell(w = 80, h = 10, txt=f"Date: {date}", border = 0, ln = 1, align = 'L', fill = False)
@@ -42,22 +42,22 @@ for filepath in filepaths:
         pdf.cell(w = 30, h = 8, txt=str(row['price_per_unit']), border = 1, align = 'L', fill = False)
         pdf.cell(w = 25, h = 8, txt=str(row['total_price']), border = 1, ln = 1, align = 'L', fill = False)
 
-    # total_sum = df.total_price.sum()
-    # pdf.set_font(family='Times', size=12)
-    # pdf.set_text_color(80, 80, 80)
-    # pdf.cell(w = 25, h = 8, txt="", border = 1, align = 'L', fill = False)
-    # pdf.cell(w = 70, h = 8, txt="", border = 1, align = 'L', fill = False)
-    # pdf.cell(w = 40, h = 8, txt="", border = 1, align = 'L', fill = False)
-    # pdf.cell(w = 30, h = 8, txt="", border = 1, align = 'L', fill = False)
-    # pdf.cell(w = 25, h = 8, txt=str(total_sum), border = 1, ln = 1, align = 'L', fill = False)
+    total_sum = df.total_price.sum()
+    pdf.set_font(family='Times', size=12, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w = 25, h = 8, txt="", border = 0, align = 'L', fill = False)
+    pdf.cell(w = 70, h = 8, txt="", border = 0, align = 'L', fill = False)
+    pdf.cell(w = 40, h = 8, txt="", border = 0, align = 'L', fill = False)
+    pdf.cell(w = 30, h = 8, txt="Total", border = 1, align = 'L', fill = False)
+    pdf.cell(w = 25, h = 8, txt=str(total_sum), border = 1, ln = 1, align = 'L', fill = False)
 
-    # # Add total sum sentence
-    # pdf.set_font(family='Times', style='B', size=12)
-    # pdf.cell(w = 25, h = 8, txt=f"The total price is {str(total_sum)}", ln = 1, align = 'L', fill = False)
+    # Add total sum sentence
+    pdf.set_font(family='Times', style='B', size=12)
+    pdf.cell(w = 25, h = 8, txt=f"The total price is {str(total_sum)}", ln = 1, align = 'L', fill = False)
 
-    # # Add company name and logo
-    # pdf.set_font(family='Times', style='B', size=12)
-    # pdf.cell(w = 25, h = 8, txt=f"PythonHow", ln = 1, align = 'L', fill = False)
-    # pdf.image("pythonhow.png", w=10)
+    # Add company name and logo
+    pdf.set_font(family='Times', style='B', size=12)
+    pdf.cell(w = 25, h = 8, txt=f"PythonHow", ln = 1, align = 'L', fill = False)
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f'pdfs/{filename}.pdf')
